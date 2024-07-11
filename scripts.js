c74885eb950c4884dcdf287ab0115a577d9710b1
BBANG-JUN01 <129255454+BBANG-JUN01@users.noreply.github.com>
2024-07-11 11:09:36 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -58,4 +58,39 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         update() {
-           
+            this.x += this.speedX;
+            this.y += this.speedY;
+
+            if (this.size > 0.2) this.size -= 0.1;
+        }
+
+        draw() {
+            ctx.fillStyle = 'rgba(255, 182, 193, 0.8)';
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+            ctx.closePath();
+            ctx.fill();
+        }
+    }
+
+    function init() {
+        particlesArray = [];
+        for (let i = 0; i < 100; i++) {
+            particlesArray.push(new Particle());
+        }
+    }
+
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        for (let i = 0; i < particlesArray.length; i++) {
+            particlesArray[i].update();
+            particlesArray[i].draw();
+        }
+
+        requestAnimationFrame(animate);
+    }
+
+    init();
+    animate();
+});
